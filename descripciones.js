a31a7557e44884dcbcc99c15d4e9dff8e6861687
powerrx2019 <powerrx2019@gmail.com>
2026-06-17 12:09:42 -0700

    Update descripciones.js

--- descripciones.js
+++ descripciones.js
@@ -171,7 +171,7 @@ const descripciones = [
   { nombre: "MUTANT WHEY DUAL", descripcion: " 22 g proteína, 10.4 g EAA, 5 g BCAA. Sin aspartame, con enzimas digestivas para optimizar resultados. 50 servicios." },
   { nombre: "NITRAFLEX", descripcion: "fórmula de alta densidad clínicamente probada con citrulina, beta alanina y arginina. Incrementa energía, concentración, fuerza, resistencia, bombeos y mejora testosterona durante el entrenamiento. 30 servicios." },
   { nombre: "NITRO TECH PERFORMANCE 4 LBS", descripcion: " proteína limpia y simple. 30 g de proteína de péptidos de suero y aislado, las más limpias y puras del mercado. 41 servicios." },
-  { nombre: "NITRO TECH WHEY GOLD 2 LBS", descripcion: " aislado y concentrado de alta calidad con péptidos. 24 g proteína + 5.5 g BCAA + 4 g glutamina. Baja en grasas y carbohidratos. 76 servicios." },
+  { nombre: "NITRO TECH WHEY GOLD 2 LBS", descripcion: " aislado y concentrado de alta calidad con péptidos. 24 g proteína + 5.5 g BCAA + 4 g glutamina. Baja en grasas y carbohidratos. 28 servicios." },
   { nombre: "NITRO TECH WHEY GOLD 5 LBS", descripcion: "24 g proteína + 5.5 g BCAA + 4 g glutamina. Rápida absorción, ideal para post-entreno. Baja en grasas y carbohidratos. 76 servicios." },
   { nombre: "NITROTECH RIPPED 4 LBS", descripcion: "promueve pérdida de grasa y mantenimiento de masa muscular. 30 g proteína + 250 mg CLA + 500 mg L-Carnitina por servicio. 42 servicios." },
   { nombre: "OMEGA 3 GAT", descripcion: "EPA y DHA de alta concentración para salud cardiovascular, función cerebral, reducción de inflamación y salud articular en atletas." },
